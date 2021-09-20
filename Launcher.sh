875b4f5ccd0beed893e2fe97bba11e184a6097c8
Bob-YsPan <46966555+Bob-YsPan@users.noreply.github.com>
2021-09-20 14:56:03 +0800

    add & update macOS Testing

--- Launcher.sh
+++ Launcher.sh
@@ -3,5 +3,5 @@ echo "| wxPython Chinese Typing Sofware Launcher"
 echo "| Bob Pan 2021/09/19"
 echo "| !!!Don't forget to install wxPython and its requirements!!!"
 echo "| Now Loading..."
-python ChineseTyping.py
+python3 ChineseTyping.py
 echo "| Script finished. Closeing..."
